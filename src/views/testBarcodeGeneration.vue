901a98f35b204dfff714671e66d6dcf86386a087
Alex <133679349+alexmen656@users.noreply.github.com>
2025-11-26 21:31:20 +0100

    feat: Try even more barcode cards and cards with other things like QR codes or even "GS1 DataBar Expanded Stacked"

--- src/views/testBarcodeGeneration.vue
+++ src/views/testBarcodeGeneration.vue
@@ -74,16 +74,90 @@
 
         <!-- how it should look-->
         <img src="https://barcode.tec-it.com/barcode.ashx?data=05wuj2%0A&code=Code128&translate-esc=on" alt=""></img>
+
+
+
+        Card 2 (02111568344951)
+        A:
+        <vue-js-barcode value="02111568344951" format="CODE128A" :height="80" :width="2"
+            display-value="true"></vue-js-barcode><!-- doesnt look like original-->
+
+        B:
+        <vue-js-barcode value="02111568344951" format="CODE128B" :height="80" :width="2"
+            display-value="true"></vue-js-barcode><!-- doesnt look like original-->
+
+        C:
+        <vue-js-barcode value="02111568344951" format="CODE128C" :height="80" :width="2"
+            display-value="true"></vue-js-barcode><!-- does actually look like original-->
+
+
+        Card 3 (2411956886828), apparantly it should be EAN13 instead of code128
+        A:
+        <vue-js-barcode value="2411956886828" format="CODE128A" :height="80" :width="2"
+            display-value="true"></vue-js-barcode><!-- doesnt look like original-->
+
+        B:
+        <vue-js-barcode value="2411956886828" format="CODE128B" :height="80" :width="2"
+            display-value="true"></vue-js-barcode><!-- doesnt look like original-->
+
+        C:
+        <vue-js-barcode value="2411956886828" format="CODE128C" :height="80" :width="2"
+            display-value="true"></vue-js-barcode><!-- doesnt look like original-->
+
+        <vue-js-barcode value="2411956886828" format="EAN13" :height="80" :width="2"
+            display-value="true"></vue-js-barcode><!-- does actually look like original-->
+
+
+        Card 4 (not barcode lol). ggs GS1 DataBar Expanded Stacked
+        ((01) 09010374 00001 9 (21) 08130755701138663732 (10) 01105944150)
+        <div style="background-color: #fff;">
+            <canvas ref="canvas"></canvas><!-- wtf this shit works??????-->
+        </div>
+
+
+        <h2>Qr code cards</h2>
+
+        1. card (4102522044):
+        <qrcode-vue value="4102522044" :size="200" level="H"></qrcode-vue><!--doesnt look right-->
+        <qrcode-vue value="4102522044" :size="200" level="L"></qrcode-vue><!--doesnt look right-->
+        <qrcode-vue value="4102522044" :size="200" level="M"></qrcode-vue><!--doesnt look right-->
+
+        <qrcode-vue value="4102 522044" :size="200" level="H"></qrcode-vue><!--doesnt look right-->
+        <qrcode-vue value="4102 522044" :size="200" level="M"></qrcode-vue><!--doesnt look right-->
+        <qrcode-vue value="4102 522044" :size="200" level="L"></qrcode-vue><!--doesnt look right-->
+
+        <!-- Apparantly it doesn't make a difference how it looks if the value is the same lol-->
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import bwipjs from 'bwip-js';
+import { onMounted, ref } from 'vue';
 import VueJsBarcode from 'vue-jsbarcode';
+import QrcodeVue from 'qrcode.vue';
 
 const pages = ref([
     { id: '05wuj2' },
 ]);
+
+
+const canvas = ref(null);
+
+onMounted(() => {
+    bwipjs.toCanvas(canvas.value, {
+        bcid: 'databarexpandedstacked',
+        //text: '(01) 09010374 00001 9 (21) 08130755701138663732 (10) 01105944150',
+        text: '(01)09010374000019(21)08130755701138663732(10)01105944150',
+        scale: 2,
+        height: 10,
+        segments: 8
+    });
+});
+/*
+bwip-js.js?v=ab36b76f:264 Uncaught (in promise) Error: bwipp.GS1valueTooLong#3540: AI 01: Too long
+    at testBarcodeGeneration.vue:131:12
+
+    */
 </script>
 
 <style scoped>
