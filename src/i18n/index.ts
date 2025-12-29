a15320039e20bdda50d6ab21613f851b06ce0b9f
Alex <133679349+alexmen656@users.noreply.github.com>
2025-12-29 20:50:40 +0100

    feat: Add multiple new languages

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,6 +4,22 @@ import en from './locales/en.json'
 import es from './locales/es.json'
 import sk from './locales/sk.json'
 import cz from './locales/cz.json'
+import fr from './locales/fr.json'
+import it from './locales/it.json'
+import pt from './locales/pt.json'
+import nl from './locales/nl.json'
+import pl from './locales/pl.json'
+import sv from './locales/sv.json'
+import da from './locales/da.json'
+import no from './locales/no.json'
+import fi from './locales/fi.json'
+import el from './locales/el.json'
+import hu from './locales/hu.json'
+import ro from './locales/ro.json'
+import uk from './locales/uk.json'
+import hr from './locales/hr.json'
+import bg from './locales/bg.json'
+import ru from './locales/ru.json'
 
 const i18n = createI18n({
   legacy: false,
@@ -15,6 +31,22 @@ const i18n = createI18n({
     es,
     sk,
     cz,
+    fr,
+    it,
+    pt,
+    nl,
+    pl,
+    sv,
+    da,
+    no,
+    fi,
+    el,
+    hu,
+    ro,
+    uk,
+    hr,
+    bg,
+    ru,
   },
 })
 
